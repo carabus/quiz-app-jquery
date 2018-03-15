d86768927ca3e1ea1f7ce75d9c253bc80ff92f4b
carabus <svetlana.myth@gmail.com>
2018-03-15 11:20:02 -0700

    Fixed issues in js from code review Added styling

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const quizAppViews = ['.js-quiz-item-feedback-section','.js-start-page', '.js-quiz-item','.js-quiz-state', '.js-feedback-page','.js-quiz-navigation'];
+const quizAppViews = ['.js-quiz-item-feedback-section','.js-start-page', 
+'.js-quiz-item', '.js-quiz-state', '.js-feedback-page','.js-quiz-navigation'];
 
-// Quiz App State - set by resetQuizAppState()
+/** Quiz App State - set by resetQuizAppState() */
 let quizApp = {
 };
 
 $(handleQuizApp);
 
+/** Displays start page and handles all application events */
 function handleQuizApp() {
   renderStartPage();
   handleStartQuiz();
@@ -14,7 +16,10 @@ function handleQuizApp() {
   handleRestartQuiz();
 }
 
-// Update current view to only show required views and hide all the rest
+/** 
+  * Update current view to only show required views and hide all the rest
+  * @param {array} viewsToShow - array of views to show  
+  */
 function updateView(viewsToShow) {
   let viewsToHide = quizAppViews.filter(function(element) {
     return !viewsToShow.includes(element);
@@ -23,36 +28,50 @@ function updateView(viewsToShow) {
   viewsToHide.forEach(element => $(element).hide());
 }
 
-// shuffle STORE array to randomize question selection
+/**
+  * Shuffle array to randomize question selection
+  * @param {array} array - array to shuffleArray
+  */
 function shuffleArray(array) {
-  let i1, i2;
-  for (i1 = array.length - 1; i1 > 0; i1--) {
-    i2 = Math.floor(Math.random() * (i1 + 1));
-    [array[i1], array[i2]] = [array[i2], array[i1]];
-  }
+    let i1, i2;
+    for (i1 = array.length - 1; i1 > 0; i1--) {
+        i2 = Math.floor(Math.random() * (i1 + 1));
+        [array[i1], array[i2]] = [array[i2], array[i1]];
+    }
 }
 
+/** Generate current quiz by selecting top 5 elements from shuffled STORE */
 function generateQuizFromStore() {
   shuffleArray(STORE);
   return STORE.slice(0,5);
 }
 
+/** Disable quiz form */
 function disableForm() {
-  $('#js-quiz-item').find(':input, :button').attr('disabled','disabled');  
+  $('#js-quiz-item').find('input, button[type="submit"]').attr('disabled','disabled');  
 }
 
+/** Enable quiz form */
 function enableForm() {
-  $('#js-quiz-item').find(':input, :button').removeAttr('disabled');
+  $('#js-quiz-item').find('input, button[type="submit"]').removeAttr('disabled');
 }
 
+/** Reset quiz form */
 function resetForm() {
   $('#js-quiz-item')[0].reset();
 }
 
+/** Reset application state when new quiz starts */
 function resetQuizAppState() {
-  quizApp = {currentQuiz: generateQuizFromStore(), questionCounter: -1, correctAnswers: 0, incorrectAnswers: 0};
+  quizApp = {
+    currentQuiz: generateQuizFromStore(), 
+    questionCounter: -1, 
+    correctAnswers: 0, 
+    incorrectAnswers: 0
+  };
 }
 
+/** Populate quiz form with current quiz question and answer options */
 function renderQuestion() {
   currentQuestion = quizApp.currentQuiz[quizApp.questionCounter];
   
@@ -61,29 +80,29 @@ function renderQuestion() {
   enableForm();
   
   // populate form with quiz question
-  $('.js-question').text(currentQuestion.question);
-  $('label[for="option-1"]').text(currentQuestion.answer1);
-  $('label[for="option-2"]').text(currentQuestion.answer2);  
-  $('label[for="option-3"]').text(currentQuestion.answer3);
-  $('label[for="option-4"]').text(currentQuestion.answer4);
+  $('.js-question').text(`Question ${quizApp.questionCounter+1} of 5: ${currentQuestion.question}`);
+  currentQuestion.answers.forEach(function(answer, index) {
+    $(`label[for="option-${index}"]`).text(answer);  
+  });
 }
 
-// New quiz set up
+/** New quiz set up */
 function startNewQuiz() {
   // reset application state
   resetQuizAppState();
-
-  // show first question
+  // process and show first question
   processNextQuestion();
   renderUserScore();   
 }
 
+/** Event handler for pressing "Start Quiz" button */
 function handleStartQuiz() {
   $('#startQuiz').on('click', function(event) {
     startNewQuiz();
   });
 }
 
+/** Event handler for Submitting an answer for current quiz question */
 function handleAnswerSubmit() {
   $('#js-quiz-item').submit(function(event) {
     event.preventDefault();
@@ -106,59 +125,80 @@ function handleAnswerSubmit() {
   });
 }
 
+/** 
+  * Compare user answer to expected quiz answer.
+  * @param {String} answer - answer given by a user
+  */
 function isCorrect(answer) {
   return answer === quizApp.currentQuiz[quizApp.questionCounter].correctAnswer;  
 }
 
+/** 
+ * Generate feedback message for quiz question answer. 
+ * Depends on whether the user answered correctly or not.
+ * @param {boolean} isCorrectAnswer
+ */
 function generateFeedbackMessage(isCorrectAnswer) {
-  return isCorrectAnswer ? 'The answer was correct. Yay!': `The answer was incorrect. Correct answer is ${quizApp.currentQuiz[quizApp.questionCounter].correctAnswer}.`;
+  return isCorrectAnswer ? 
+  'The answer was correct.': 
+  `The answer was incorrect. Correct answer is ${quizApp.currentQuiz[quizApp.questionCounter].correctAnswer}.`;
 } 
 
+/**
+ * Update user score for current quiz.
+ * Depends on whether the user answered correctly or not.
+ * @param {boolean} isCorrectAnswer
+ */
 function updateUserScore(isCorrectAnswer) {
   if(isCorrectAnswer) quizApp.correctAnswers++;
   else quizApp.incorrectAnswers++;
 }
 
+/** Display quiz question feedback on the screen
+ * @param {String} message - message to Display
+ */
 function renderQuestionFeedback(message) {
   $('.js-quiz-item-feedback').text(message);
   updateView(['.js-quiz-item-feedback-section', '.js-quiz-item','.js-quiz-state', '.js-quiz-navigation']);
 }
 
+/** Display current user score on the screen */
 function renderUserScore() {
   console.log(`Current user score - ${quizApp.correctAnswers}, ${quizApp.incorrectAnswers}`);
-  $('.js-quiz-current-score').text(`${quizApp.correctAnswers} correct, ${quizApp.incorrectAnswers} incorrect`);
+  $('.correct').text(quizApp.correctAnswers);
+  $('.incorrect').text(quizApp.incorrectAnswers);
 }
 
-function updateQuizProgress() {
-  $('.js-quiz-progress').text(`Question ${quizApp.questionCounter+1} out of 5`);
-}
-
+/** Display Start Page section */
 function renderStartPage() {
   updateView(['.js-start-page']);
 }
 
+/** Handle iteration over quiz questions*/
 function processNextQuestion() {
   // update the counter
   quizApp.questionCounter++;
   
   // if it is the last question - display feedback page instead
   if (quizApp.questionCounter===quizApp.currentQuiz.length) {
-    renderFeedbackPage();
-    return;
+      renderFeedbackPage();
+      return;
   }
 
   // display next question
   renderQuestion();
-  updateQuizProgress();
+  // updateQuizProgress();
   updateView(['.js-quiz-item','.js-quiz-state']);
 }
 
+/** Handle "Next Question" button pressed by the user*/
 function handleNextQuestion() {
   $('#js-next-question').on('click', function(event) {
     processNextQuestion();
   });
 }
 
+/** Populate and display quiz feedback page. */
 function renderFeedbackPage() {
   // update feedback message with current score
   $('.js-quiz-feedback').text(`You've got ${quizApp.correctAnswers} out 5 questions right. Great Job!`);
@@ -166,6 +206,7 @@ function renderFeedbackPage() {
   updateView(['.js-feedback-page']);
 }
 
+/** Handle "Restart Quiz" button pressed by the user. */
 function handleRestartQuiz() {
   $('#restartQuiz').on('click', function(event) {
     startNewQuiz();
